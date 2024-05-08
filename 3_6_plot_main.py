2cd43cc51da7840f0190627ed87d107d2d8f4be9
Max <kotzmax@gmail.com>
2024-05-08 13:58:04 +0200

    finished plot

--- 3_6_plot_main.py
+++ 3_6_plot_main.py
@@ -3,9 +3,41 @@ import numpy as np
 from typing import List
 import napari
 import os
+import matplotlib.pyplot as plt
 
 from config import *
 from IO import *
+
+def plot_img_res(mesh,proj,labels,sig_img,scales):
+    unique_integers = np.unique(labels)
+    colors = plt.cm.get_cmap('nipy_spectral', len(unique_integers))
+    np.random.seed(42//2) 
+    indices = np.arange(len(unique_integers))
+    np.random.shuffle(indices)
+    color_mapping = {int_val: colors(idx / len(unique_integers)) for idx, int_val in zip(indices, unique_integers)}
+    vertex_colors = np.array([color_mapping[val] for val in labels])
+    viewer = napari.Viewer(ndisplay=3)
+    vertices = mesh.points/scales
+    faces = mesh.faces.reshape((-1, 4))[:, 1:4]
+    viewer.add_surface((vertices, faces),vertex_colors=vertex_colors, name='Seg res')
+    viewer.add_surface((vertices, faces, proj), name='proj')
+    viewer.add_image(sig_img, name='img')
+    napari.run()
+
+def plot_res(mesh,proj,labels):
+    unique_integers = np.unique(labels)
+    colors = plt.cm.get_cmap('nipy_spectral', len(unique_integers))
+    np.random.seed(42//2) 
+    indices = np.arange(len(unique_integers))
+    np.random.shuffle(indices)
+    color_mapping = {int_val: colors(idx / len(unique_integers)) for idx, int_val in zip(indices, unique_integers)}
+    vertex_colors = np.array([color_mapping[val] for val in labels])
+    viewer = napari.Viewer(ndisplay=3)
+    vertices = mesh.points
+    faces = mesh.faces.reshape((-1, 4))[:, 1:4]
+    viewer.add_surface((vertices, faces),vertex_colors=vertex_colors, name='Seg res')
+    viewer.add_surface((vertices, faces, proj), name='proj')
+    napari.run()
 
 def plot():
     signal_dir_path=os.path.join(EpFlat_path,'signal_upperlayer')
@@ -41,10 +73,13 @@ def plot():
         signal_path=os.path.join(signal_dir_path,fin_folder+'.tif')
         sig_img=getImage(signal_path)
 
-        print(scales)
-        print(proj)
-        print(labels)
-        print(sig_img)
+        # print(scales)
+        # print(proj)
+        # print(labels)
+        # print(sig_img)
+
+        #plot_img_res(mesh,proj,labels,sig_img,scales)
+        plot_res(mesh,proj,labels)
 
 if __name__ == "__main__":
     plot() 
