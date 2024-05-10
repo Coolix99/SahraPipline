9eedc5d1ff3d9056ba3940d31659c84ac635d7d4
Max <kotzmax@gmail.com>
2024-05-10 11:28:56 +0200

    add_labels

--- 1_1_Orientation_main.py
+++ 1_1_Orientation_main.py
@@ -19,7 +19,7 @@ def extract_coordinate(df, name):
 
 def orient_session(im):
     viewer = napari.Viewer(ndisplay=3)
-    im_layer = viewer.add_image(im,rendering='iso')
+    im_layer = viewer.add_labels(im)
     last_pos=None
     last_viewer_direction=None
     points = []
