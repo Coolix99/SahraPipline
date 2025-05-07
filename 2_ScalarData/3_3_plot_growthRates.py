361b9225bb8f9bc7bb3d1d59696c54207eab62d3
Max <kotzmax@gmail.com>
2025-05-07 15:51:21 +0200

    Refactor code structure for improved readability and maintainability

--- 2_ScalarData/3_3_plot_growthRates.py
+++ 2_ScalarData/3_3_plot_growthRates.py
@@ -2,20 +2,18 @@ import pandas as pd
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import UnivariateSpline
 
-from plotHelper.plotBokehHelper_old import plot_scatter_corner,plot_double_timeseries_II,add_data_to_plot_II,add_fit_to_plot_II
-from bokeh.io import show
-from bokeh.plotting import figure
-
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from config import *
 
 
-
 def getData():
-    hdf5_file_path = os.path.join(Curv_Thick_path, 'scalarGrowthData.h5')
+    df_file_path = os.path.join(scalar_path,'scalarGrowthData.csv')
 
     # Load the DataFrame from the HDF5 file
-    df = pd.read_hdf(hdf5_file_path, key='data')
+    df = pd.read_csv(df_file_path,sep=';')
 
     # Calculate new columns:
     # L AP * L PD
@@ -30,20 +28,18 @@ def getData():
     df['log L AP'] = np.log(df['L AP'])
     df['log L PD'] = np.log(df['L PD'])
 
-
     return df
 
 
-from scipy.interpolate import UnivariateSpline
 
 def fit_and_sample_derivatives(df: pd.DataFrame, column: str, N: int = 200,s: float = 0):
     results = {}
     grouped = df.groupby('condition')
 
     for condition, group in grouped:
-        if  condition == '48FF_cut' or condition == '72FF_cut':
+        if  condition == '4850cut' or condition == '7230cut':
             continue
-        
+
         # Calculate mean and std for each time
         stats = group.groupby('time in hpf')[column].agg(['mean', 'std']).reset_index()
 
@@ -60,7 +56,6 @@ def fit_and_sample_derivatives(df: pd.DataFrame, column: str, N: int = 200,s: fl
             # Sample around mean with std
             sampled_values = np.random.normal(stats['mean'], stats['std']/2)
             # Fit a UnivariateSpline
-            
             spline = UnivariateSpline(stats['time in hpf'], sampled_values,w=1/stats['std'], s=s,k=2)
             fitted_values = spline(dense_t)
             
@@ -142,129 +137,28 @@ def main():
     df=getData()
     print(df.columns)
 
+    res=fit_and_sample_derivatives(df, 'Volume', N=1000,s=0.4e1)
+    fit_results_dev = {
+        't_values': res['Development']['time'],
+        'fits': res['Development']['fitted_values']
+    }
+    fit_results_reg = {
+        't_values': res['Regeneration']['time'],
+        'fits': res['Regeneration']['fitted_values']
+    }
+    plot_fit_with_uncertainty(
+        fit_results_list=[fit_results_dev, fit_results_reg],
+        colors=['blue', 'orange'],
+        labels=['Development', 'Regeneration'],
+        title='Volume ',
+        xlabel='t [hpf]',
+        ylabel=r'$\dot{V} \quad [\mu m^3]$',
+        # ymin=0,
+        # xmin=48,
+        # dx=12,
+        # dy=10000,
+    )
 
-    # res=fit_and_sample_derivatives(df, 'Volume', N=500,s=0.5e1)
-    # fit_results_dev = {
-    #     't_values': res['Development']['time'],
-    #     'fits': res['Development']['fitted_values']
-    # }
-    # fit_results_reg = {
-    #     't_values': res['Regeneration']['time'],
-    #     'fits': res['Regeneration']['fitted_values']
-    # }
-
-    # fit_results_4850 = {
-    #     't_values': res['4850cut']['time'],
-    #     'fits': res['4850cut']['fitted_values']
-    # }
-
-    # p,width=plot_double_timeseries_II(df,categories=('Development','Regeneration'), y_col='Surface Area', style='violin',y_scaling=1,y_name=r'Area $$(\mu m)^2$$',test_significance=True,y0=0)
-    # #show(p)
-    # p = add_fit_to_plot_II(p, fit_results_dev, color='blue',label='Development')  
-    # p = add_fit_to_plot_II(p, fit_results_reg, color='orange',label='Regeneration')  
-    # show(p)
-
-    # p = figure(title=f"Surface Area growth rate",
-    #            x_axis_label=r'time in hpf',
-    #            y_axis_label=r'dArea/dt $$(\mu m)^2/h$$',
-    #            width=700, height=400,
-    #            tools="pan,wheel_zoom,box_zoom,reset,save")
-    # fit_results_dev = {
-    #     't_values': res['Development']['time'],
-    #     'fits': res['Development']['derivative']
-    # }
-    # fit_results_reg = {
-    #     't_values': res['Regeneration']['time'],
-    #     'fits': res['Regeneration']['derivative']
-    # }
-    # p = add_fit_to_plot_II(p, fit_results_dev, color='blue',label='Development')  
-    # p = add_fit_to_plot_II(p, fit_results_reg, color='orange',label='Regeneration')  
-    # show(p)
-    # import time
-    # time.sleep(2.5)
-    # p = figure(title=f"relative Surface Area growth rate",
-    #            x_axis_label=r'time in hpf',
-    #            y_axis_label=r'dArea/dt/Area $$1/h$$',
-    #            width=700, height=400,
-    #            tools="pan,wheel_zoom,box_zoom,reset,save")
-    # fit_results_dev = {
-    #     't_values': res['Development']['time'],
-    #     'fits': res['Development']['relative_derivative']
-    # }
-    # fit_results_reg = {
-    #     't_values': res['Regeneration']['time'],
-    #     'fits': res['Regeneration']['relative_derivative']
-    # }
-    # p = add_fit_to_plot_II(p, fit_results_dev, color='blue',label='Development')  
-    # p = add_fit_to_plot_II(p, fit_results_reg, color='orange',label='Regeneration')  
-    # show(p)
-    
-    # #endregion
-
-    # #region Volume
-    # res=fit_and_sample_derivatives(df, 'Volume', N=500,s=0.5e1)
-    
-    # fit_results_dev = {
-    #     't_values': res['Development']['time'],
-    #     'fits': res['Development']['fitted_values']
-    # }
-    # fit_results_reg = {
-    #     't_values': res['Regeneration']['time'],
-    #     'fits': res['Regeneration']['fitted_values']
-    # }
-
-    # p,width=plot_double_timeseries_II(df,categories=('Development','Regeneration'), y_col='Volume', style='violin',y_scaling=1,y_name=r'Volume $$(\mu m)^3$$',test_significance=True,y0=0)
-    # show(p)
-    # p = add_fit_to_plot_II(p, fit_results_dev, color='blue',label='Development')  
-    # p = add_fit_to_plot_II(p, fit_results_reg, color='orange',label='Regeneration')  
-    # show(p)
-
-    # p = figure(title=f"Volume growth rate",
-    #            x_axis_label=r'time in hpf',
-    #            y_axis_label=r'dV/dt $$(\mu m)^3/h$$',
-    #            width=700, height=400,
-    #            tools="pan,wheel_zoom,box_zoom,reset,save")
-    # fit_results_dev = {
-    #     't_values': res['Development']['time'],
-    #     'fits': res['Development']['derivative']
-    # }
-    # fit_results_reg = {
-    #     't_values': res['Regeneration']['time'],
-    #     'fits': res['Regeneration']['derivative']
-    # }
-    # p = add_fit_to_plot_II(p, fit_results_dev, color='blue',label='Development')  
-    # p = add_fit_to_plot_II(p, fit_results_reg, color='orange',label='Regeneration')  
-    # show(p)
-    # import time
-    # time.sleep(2.5)
-    # p = figure(title=f"relative Volume growth rate",
-    #            x_axis_label=r'hpf [t]',
-    #            y_axis_label=r'$$\dot{V}/V \quad [1/h]$$',
-    #            width=700, height=400,
-    #            tools="pan,wheel_zoom,box_zoom,reset,save")
-    
-    # fit_results_dev = {
-    #     't_values': res['Development']['time'],
-    #     'fits': res['Development']['relative_derivative']
-    # }
-    # fit_results_reg = {
-    #     't_values': res['Regeneration']['time'],
-    #     'fits': res['Regeneration']['relative_derivative']
-    # }
-
-    # fit_results_4850 = {
-    #     't_values': res['4850cut']['time'],
-    #     'fits': res['4850cut']['relative_derivative']
-    # }
-    # p = add_fit_to_plot_II(p, fit_results_dev, color='blue',label='Development')  
-    # p = add_fit_to_plot_II(p, fit_results_reg, color='orange',label='Regeneration')  
-    # p = add_fit_to_plot_II(p, fit_results_4850, color='black',label='48hpf 50%')  
-    # p.xaxis.axis_label_text_font_size = "14pt"
-    # p.yaxis.axis_label_text_font_size = "14pt"
-    # p.xaxis.major_label_text_font_size = "12pt"
-    # p.yaxis.major_label_text_font_size = "12pt"
-    # show(p)
-    res=fit_and_sample_derivatives(df, 'Volume', N=1000,s=0.5e1)
     fit_results_dev = {
         't_values': res['Development']['time'],
         'fits': res['Development']['derivative']
@@ -350,14 +244,7 @@ def main():
         dy=0.02,
     )
 
-    #endregion
 
-    # plot_single_timeseries(df, filter_col='condition', filter_value='Regeneration', y_col='Volume', style='violin', color='orange',width=None)
-    # plot_double_timeseries(df, y_col='Volume', style='violin',y_scaling=1e-6,y_name=r'Tissue Volume $$10^6 \mu m^3$$',test_significance=True,y0=0)
-    # plot_double_timeseries(df, y_col='Surface Area', style='violin',y_scaling=1e-4,y_name=r'Area $$(100 \mu m)^2$$',test_significance=True,y0=0)
-    # plot_double_timeseries(df, y_col='L PD', style='violin',y_scaling=1,y_name=r'$$L_{DP} in \mu m$$',test_significance=True,y0=0)
-    # plot_double_timeseries(df, y_col='L AP', style='violin',y_scaling=1,y_name=r'$$L_{AP} in \mu m$$',test_significance=True,y0=0)
-    # plot_double_timeseries(df, y_col='L DV', style='violin',y_scaling=1,y_name=r'$$L_{DV} in \mu m$$',test_significance=True,y0=0)
     
     
 if __name__ == "__main__":
